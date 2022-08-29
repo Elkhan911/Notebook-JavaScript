2c552be7f90fe9ada817bc8c0711b241d1d13e63
Elkhan911 <955pro@mail.ru>
2022-08-29 07:50:18 +0500

    added data-text for list items at html and js

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ let arrOfNotes = ["note1", "note2", "note3", "note4"];
 
 saveBtn.addEventListener("click", createNote);
 
-//функция создания новой записи
+// функция создания новой записи
 function createNote() {
   if (textZone.value !== "") {
     let li = document.createElement("li");
     li.classList.add("notebook__item");
     notesCounter++;
     li.textContent = "note" + notesCounter;
+    li.setAttribute("data-text", textZone.value);
     list.append(li);
 
     arrOfNotes.push(li.textContent);
