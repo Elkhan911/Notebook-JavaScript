6f7d2ad14efa92af1ddee09e6da42b805e27b197
Elkhan911 <955pro@mail.ru>
2022-08-29 08:27:23 +0500

    function for reading default notes

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const notebook = document.querySelector("#_notebook");
 const saveBtn = document.querySelector("#_saveBtn");
 const newNoteBtn = document.querySelector("#_newNoteBtn");
 let textZone = document.querySelector("#_textZone");
+const notebookItems = document.querySelectorAll(".notebook__item");
 
 let list = document.querySelector("#_list");
 let notesCounter = 4;
-let arrOfNotes = ["note1", "note2", "note3", "note4"];
+let arrOfNotes = ["Запись 1", "Запись 2", "Запись 3", "Запись 4"];
 
 saveBtn.addEventListener("click", createNote);
 
@@ -15,13 +16,29 @@ function createNote() {
     let li = document.createElement("li");
     li.classList.add("notebook__item");
     notesCounter++;
-    li.textContent = "note" + notesCounter;
+    li.textContent = "Запись " + notesCounter;
     li.setAttribute("data-text", textZone.value);
     list.append(li);
 
     arrOfNotes.push(li.textContent);
     console.log(arrOfNotes);
 
-    textZone.value = "";
+    // решить вопрос чтоб при редактировании записи сохраниась отредактированная версия а не создавалась новая
+    li.addEventListener("click", getNoteText);
+  }
+  textZone.value = "";
+}
+
+// функция для записи в блокнот
+function getNoteText() {
+  textZone.value = this.getAttribute("data-text");
+}
+
+// функция для чтения записей, которые даны по умолчанию
+function getTextForDeafualtNotes() {
+  for (let notebookItem of notebookItems) {
+    notebookItem.addEventListener("click", getNoteText);
   }
 }
+
+getTextForDeafualtNotes();
