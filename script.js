d191d3c346d88a4084a805b77b868e93a4f87cc0
Elkhan911 <955pro@mail.ru>
2022-09-18 18:32:34 +0500

    fix js functions - add, open and save notes

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ const list = document.querySelector("#_list");
 const newNoteBtn = document.querySelector("#_newNoteBtn");
 const textZone = document.querySelector("#_textZone");
 const saveBtn = document.querySelector("#_saveBtn");
+
+let arrOfNotes = ["Запись 1", "Запись 2", "Запись 3", "Запись 4"];
+let noteCounter = 1;
+let currentNote;
+
+for (let i = 0; i < arrOfNotes.length; i++) {
+  addNote();
+}
+
+function addNote() {
+  let li = document.createElement("li");
+  li.classList.add("notebook__item");
+  li.setAttribute("data-text", `Запись ${noteCounter}`);
+  li.textContent = `Запись ${noteCounter}`;
+  noteCounter++;
+  list.append(li);
+  li.addEventListener("click", openNote);
+}
+
+function openNote(event) {
+  currentNote = event.target;
+  textZone.value = currentNote.getAttribute("data-text");
+}
+
+function saveNote(event) {
+  currentNote.setAttribute("data-text", textZone.value);
+}
+
+saveBtn.addEventListener("click", saveNote);
+newNoteBtn.addEventListener("click", addNote);
